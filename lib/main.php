f2940241ada13060d979a73aa4e7cd7af8e58174
Dmitry Fedyuk <dfediuk@gmail.com>
2018-08-16 05:20:59 +0600

    0.0.3: https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/1

---
+++ lib/main.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 2018-08-16
+ * @param string $v
+ * @return string
+ */
+function ikf_eti($v) {return $v;}
+
+/**
+ * 2018-08-16
+ * @param int|string $v
+ * @return string
+ */
+function ikf_ite($v) {return $v;}
