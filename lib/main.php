12cefc5b2028c1200986730017bf7013e8df7faf
Dmitry Fedyuk <dfediuk@gmail.com>
2018-08-16 06:00:20 +0600

    0.0.6

--- lib/main.php
+++ lib/main.php
@@ -1,5 +1,6 @@
 <?php
 use Magento\Sales\Model\Order as O;
+use Magento\Store\Model\Store as S;
 /**
  * 2018-08-16
  * «Modify orders numeration for Mediaclip»
@@ -15,10 +16,10 @@ function ikf_eti($v) {return df_last(explode('-', $v));}
  * «Modify orders numeration for Mediaclip»
  * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/1
  * @used-by \Mangoit\MediaclipHub\Observer\CheckoutSuccess::post()
- * @param int|string $v
- * @param O|null $o [optional]
+ * @param int|string|O $v
  * @return string
  */
-function ikf_ite($v, $o = null) {return dfcf(function($v, $o) {/** @var O|null $o */ return
-	!df_contains(df_cfg('api/api_auth/api_id', $o ? $o->getStore() : null), 'staging') ? $v : "staging-$v"
-;}, [$v, $o]);}
+function ikf_ite($v) {return dfcf(function($v) {
+	list($v, $s) = $v instanceof O ? [$v->getId(), $v->getStore()] : [$v, null]; /** @var S|null $s */
+	return !df_contains(df_cfg('api/api_auth/api_id', $s), 'staging') ? $v : "staging-$v";
+}, [$v]);}
