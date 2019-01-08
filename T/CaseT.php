5d9c9868bfba0c3896925656abfca6c86f657a19
Dmitry Fedyuk <dfediuk@gmail.com>
2019-01-08 17:45:31 +0700

    no message

---
+++ T/CaseT.php
@@ -0,0 +1,5 @@
+<?php
+namespace Inkifi\Core\T;
+// 2019-01-08
+/** @see \Inkifi\Core\T\CaseT\Mediaclip */
+abstract class CaseT extends \Df\Core\TestCase {}
---
+++ T/CaseT/Mediaclip.php
@@ -0,0 +1,12 @@
+<?php
+namespace Inkifi\Core\T\CaseT;
+// 2019-01-08
+final class Mediaclip extends \Inkifi\Core\T\CaseT {
+	/** 2019-01-08 */
+	function t00() {}
+
+	/** @test 2019-01-08 */
+	function t01() {
+		
+	}
+}
