e873b2e72c0eeeada90e418addfdec768043c5e0
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-13 21:18:23 +0600

    0.1.8

--- lib/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Exception as E;
-use Magento\Framework\DataObject;
-/**
- * 2018-09-11
- * @used-by \Mangoit\MediaclipHub\Controller\Index\RenewMediaclipToken::execute()
- * @param DataObject|mixed[]|mixed|E $v
- */
-function ikf_log($v) {df_log_l('Inkifi_Core', $v); df_sentry('Inkifi_Core', $v);}
